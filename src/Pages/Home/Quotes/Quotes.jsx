cc1516ea0f9ba23ff08b7a5e85ed89d3434c5c9c
Jahidul37 <jahidul.islam6610@gmail.com>
2023-09-27 12:54:23 +0600

    code adds quotes

--- src/Pages/Home/Quotes/Quotes.jsx
+++ src/Pages/Home/Quotes/Quotes.jsx
@@ -1,5 +1,6 @@
 
 import React, { useEffect } from 'react';
+import { FaQuoteLeft } from 'react-icons/fa';
 import AOS from "aos";
 const Quotes = () => {
 
@@ -14,11 +15,11 @@ const Quotes = () => {
             data-aos-offset="300"
             data-aos-easing="ease-in-sine"
 
-            className=" card-body m-3 rounded-lg my-10 py-4 bg-slate-50  relative">
+            className=" card-body m-3 rounded-lg my-10 py-6 bg-slate-50  relative">
             <div className="content text-center ">
-                <div className="max-w-md">
+                <div className="max-w-md mx-auto">
                     <h1 className="text-3xl font-bold text-amber-300 mb-4">Quotes</h1>
-                    <span className='text-5xl'>"</span>
+                    <span className='text-5xl my-5 '><FaQuoteLeft className=' mx-auto'></FaQuoteLeft></span>
                     <p className="py-3 text-xl font-bold m-2">"If you want to live a happy life, tie it to a goal, not to people or things."</p>
                     <h4 className='text-2xl font-semibold'> — Albert Einstein —</h4>
                 </div>
